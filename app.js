bb61f012b341bc1e007642280f0139aa7e7399fa
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gail.com>
2022-09-02 20:43:51 +0530

    updating bridge backend

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
 app.use(express.json());
-
+import "./EventListenerService/NFTBinanceSmartChainEvents.js";
+import "./EventListenerService/NFTPolygonEvents.js";
+import "./EventListenerService/NFTEthereumEvents.js";
 
 process.on("uncaughtException", function (err) {
   //Added this just for testing
